e2b9f1c3b253697419f25826b4aa704138cdf0b4
dnvaishnavi <dnvaishnavi5@gmail.com>
2025-11-02 15:54:15 +0530

    frontend setup

--- grievences/src/main/java/com/example/grievences/model/User.java
+++ grievences/src/main/java/com/example/grievences/model/User.java
@@ -9,12 +9,14 @@ public class User {
     private Long id;
     private String username;
     private String password;
+    private String email;
     private String role; // "STUDENT" or "ADMIN"
 
     public User() {}
-    public User(String username, String password, String role) {
+    public User(String username, String password,String email, String role) {
         this.username = username;
         this.password = password;
+        this.email=email;
         this.role = role;
     }
 
@@ -24,6 +26,8 @@ public class User {
     public void setUsername(String username) { this.username = username; }
     public String getPassword() { return password; }
     public void setPassword(String password) { this.password = password; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
     public String getRole() { return role; }
     public void setRole(String role) { this.role = role; }
 }
